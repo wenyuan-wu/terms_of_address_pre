07b607206095241743030e59769a923e4f92a1aa
wenyuan <wenyuan.wu@uzh.ch>
2021-11-29 05:55:47 +0100

    word cloud code updated

--- word_cloud.py
+++ word_cloud.py
@@ -1,1 +1,75 @@
+#!/usr/bin/env python
+"""
+Masked wordcloud
+================
+Using a mask you can generate wordclouds in arbitrary shapes.
+"""
 
+from os import path
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from wordcloud import WordCloud, STOPWORDS
+
+from spacy.lang.en import English
+
+# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
+d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
+
+# Read the whole text.
+text = open(path.join(d, 'papers_plain_text', 'De Oliveira (2013).txt')).read()
+
+# Load English tokenizer, tagger, parser, NER and word vectors
+nlp = English()
+
+#  "nlp" Object is used to create documents with linguistic annotations.
+my_doc = nlp(text)
+
+# Create list of word tokens
+token_list = []
+for token in my_doc:
+    token_list.append(token.text)
+
+from spacy.lang.en.stop_words import STOP_WORDS
+
+# Create list of word tokens after removing stopwords
+filtered_sentence =[]
+
+for word in token_list:
+    lexeme = nlp.vocab[word]
+    if not lexeme.is_stop:
+        filtered_sentence.append(word)
+
+# print(token_list)
+# print(filtered_sentence)
+
+# read the mask image
+# taken from
+# http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg
+# alice_mask = np.array(Image.open(path.join(d, "conputer_png.png")))
+
+# stopwords = set(STOPWORDS)
+# stopwords.add("said")
+
+# wc = WordCloud(background_color="white", max_words=2000, mask=alice_mask,
+#                stopwords=stopwords, contour_width=3, contour_color='steelblue')
+
+wc = WordCloud(background_color="white", max_words=2000,
+               contour_width=3, contour_color='steelblue', width=1600, height=900)
+
+# generate word cloud
+wc.generate(" ".join(filtered_sentence))
+
+# store to file
+wc.to_file(path.join(d, "word_cloud_de_oliveira.png"))
+
+# show
+# plt.imshow(wc, interpolation='bilinear')
+# plt.axis("off")
+# plt.figure()
+# plt.imshow(alice_mask, cmap=plt.cm.gray, interpolation='bilinear')
+# plt.axis("off")
+# # plt.show()
+# plt.savefig('word_cloud.png')
